b8810f5c01b92d26bf60bca9a11352c1aea8bf8c
Blaise Ishimwe <bishimwe84@gmail.com>
2022-08-30 03:40:53 -0400

    added a comment in scripts.js for testing github

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Add a comment to test Git
+
 document
   .querySelector("#details_form")
   .addEventListener("submit", handleFormSubmit);
@@ -13,154 +15,130 @@ function handleFormSubmit(event) {
 
   console.log(productDescription);
 
-resetFormValues(event.target);
+  resetFormValues(event.target);
 
-var shoppingCard = createShoppingCard(
-   productName,
-   productQuantity,
-   productPhoto,
-   productDescription,
+  var shoppingCard = createShoppingCard(
+    productName,
+    productQuantity,
+    productPhoto,
+    productDescription
+  );
 
-);
+  var actualList = document.getElementById("shopping_container");
 
-var actualList = document.getElementById("shopping_container");
+  //After creating the first item on the list change fro, Any other... to My list
 
+  if (actualList.children.length === 0) {
+    document.getElementById("title").innerHTML = "My List";
+  }
 
-//After creating the first item on the list change fro, Any other... to My list
-
-if (actualList.children.length === 0){
-document.getElementById("title").innerHTML = "My List";
+  document.getElementById("shopping_container").appendChild(shoppingCard);
 }
-
-document.getElementById("shopping_container")
-.appendChild(shoppingCard);
-
-}
-    
 
 // after submitting and creating the card let the form refresh to be able to hold another item
 
-function resetFormValues(form){
-    for(var i = 0; i < form.length; i++){
-        form.elements[i].value = "";
-    }
+function resetFormValues(form) {
+  for (var i = 0; i < form.length; i++) {
+    form.elements[i].value = "";
+  }
 }
 
 //styling the card: size,height and display
 
-function createShoppingCard(name, quantity, photoUrl, description){
-var card = document.createElement("div");
-card.setAttribute("class", "card");
-card.style.width = "15rem";
-card.style.height = "fit-content";
-card.style.margin = "20px";
+function createShoppingCard(name, quantity, photoUrl, description) {
+  var card = document.createElement("div");
+  card.setAttribute("class", "card");
+  card.style.width = "15rem";
+  card.style.height = "fit-content";
+  card.style.margin = "20px";
 
-var img = document.createElement("img");
-img.setAttribute("class", "card-img-top");
-img.setAttribute("alt", name);
+  var img = document.createElement("img");
+  img.setAttribute("class", "card-img-top");
+  img.setAttribute("alt", name);
 
-// if the user doesn't add a photo url, use this constant photo url
+  // if the user doesn't add a photo url, use this constant photo url
 
-var constantPhotoUrl = "C:\Users\desktop\Desktop\js\bball.jpg";
+  var constantPhotoUrl = "C:UsersdesktopDesktopjs\bball.jpg";
 
-if (photoUrl.length === 0){
+  if (photoUrl.length === 0) {
     setAttribute("src", constantPhotoUrl);
-} else {
+  } else {
     img.setAttribute("src", photoUrl);
-}
-card.appendChild(img);
+  }
+  card.appendChild(img);
 
+  var cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
 
-var cardBody = document.createElement("div");
-cardBody.setAttribute("class", "card-body");
-  
+  var cardTitle = document.createElement("h5");
+  cardTitle.setAttribute("class", "card-title");
+  cardTitle.innerText = name;
+  cardBody.appendChild(cardTitle);
 
-var cardTitle = document.createElement("h5");
-cardTitle.setAttribute("class", "card-title");
-cardTitle.innerText = name;
-cardBody.appendChild(cardTitle);
+  var cardSubtitle = document.createElement("h6");
+  cardSubtitle.setAttribute("class", "card-subtitle mb-2 text-muted");
+  cardSubtitle.innerText = quantity;
+  cardBody.appendChild(cardSubtitle);
 
-var cardSubtitle = document.createElement("h6");
-cardSubtitle.setAttribute("class", "card-subtitle mb-2 text-muted");
-cardSubtitle.innerText = quantity;
-cardBody.appendChild(cardSubtitle);
-
-
-if (description !== 0){
+  if (description !== 0) {
     var cardText = document.createElement("p");
     cardText.setAttribute("class", "card-text");
     cardText.innerText = description;
     cardBody.appendChild(cardText);
-}
+  }
 
+  // add bootstrap buttons to the card itself
+  var buttonContainer = document.createElement("div");
+  buttonContainer.setAttribute("class", "button_container");
 
+  var cardEditBtn = document.createElement("button");
+  cardEditBtn.setAttribute("class", "btn btn-warning");
+  cardEditBtn.innerText = "Edit";
+  cardEditBtn.addEventListener("click", editProduct);
 
-// add bootstrap buttons to the card itself
-var buttonContainer = document.createElement("div");
-buttonContainer.setAttribute("class", "button_container");
+  var cardDeleteBtn = document.createElement("button");
+  cardDeleteBtn.setAttribute("class", "btn btn-danger");
+  cardDeleteBtn.innerText = "Remove";
+  cardDeleteBtn.addEventListener("click", removeProduct);
 
+  buttonContainer.appendChild(cardEditBtn);
+  buttonContainer.appendChild(cardDeleteBtn);
 
-var cardEditBtn = document.createElement("button");
-cardEditBtn.setAttribute("class", "btn btn-warning");
-cardEditBtn.innerText = "Edit";
-cardEditBtn.addEventListener("click", editProduct);
+  cardBody.appendChild(buttonContainer);
 
+  card.appendChild(cardBody);
 
-var cardDeleteBtn = document.createElement("button");
-cardDeleteBtn.setAttribute("class", "btn btn-danger");
-cardDeleteBtn.innerText = "Remove";
-cardDeleteBtn.addEventListener("click", removeProduct);
-
-buttonContainer.appendChild(cardEditBtn);
-buttonContainer.appendChild(cardDeleteBtn);
-
-
-cardBody.appendChild(buttonContainer);
-
-card.appendChild(cardBody);
-
-return card;
-
+  return card;
 }
 
 // incase the user wants to the a card let them do it as an alert
-function editProduct(event){
-    var cardBody = event.target.parentElement.parentElement;
-    var title = cardBody.children[0];
-    var subTitle = cardBody.children[1];
+function editProduct(event) {
+  var cardBody = event.target.parentElement.parentElement;
+  var title = cardBody.children[0];
+  var subTitle = cardBody.children[1];
 
+  var card = cardBody.parentElement;
+  var photoUrl = card.children[0];
 
-    var card = cardBody.parentElement;
-    var photoUrl = card.children[0];
+  var newTitle = window.prompt("Enter New Name");
+  var newSubTitle = window.prompt("Enter New Quantity");
+  var newPhotoUrl = window.prompt("Enter New photo Url");
 
+  if (newTitle.length > 0) {
+    title.innerTitle = newTitle;
+  }
 
-    var newTitle = window.prompt("Enter New Name");
-    var newSubTitle = window.prompt("Enter New Quantity");
-    var newPhotoUrl = window.prompt("Enter New photo Url");
+  if (newSubTitle.length > 0) {
+    subTitle.innerTitle = newSubTitle;
+  }
 
-
-    if (newTitle.length > 0){
-        title.innerTitle = newTitle;
-    }
-
-
-    
-    if (newSubTitle.length > 0){
-        subTitle.innerTitle = newSubTitle;
-    }
-
-
-    
-    if (newPhotoUrl.length > 0){
-        photoUrl.setAttribute("src", newPhotoUrl);
-    }
+  if (newPhotoUrl.length > 0) {
+    photoUrl.setAttribute("src", newPhotoUrl);
+  }
 }
 
-
-function removeProduct(event){
-    var cardBody = event.target.parentElement.parentElement;
-    var card = cardBody.parentElement;
-    card.remove(); 
+function removeProduct(event) {
+  var cardBody = event.target.parentElement.parentElement;
+  var card = cardBody.parentElement;
+  card.remove();
 }
-
-
